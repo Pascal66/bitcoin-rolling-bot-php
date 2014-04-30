e984390147ed27a62fadcaea6935e873c6d4f4fb
pikespeak <olivermark83@gmail.com>
2014-04-30 16:41:03 +0200

    Changed Readme

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ bitcoin-rolling-bot-php
 Support Thread: https://bitcointalk.org/index.php?topic=366546.0
 
 BTC-e Multi Engine rolling bot with stop loss
+
+
+Todo
+
+- Cleanup Code
+- Better and More Comments
+- Dokumentation
+- Tests
+- Optimize Bots
+- Add other Currencys 
+- Test Stop Loss
--- config.php
+++ config.php
@@ -21,8 +21,8 @@ $maxBTC = 1; //The maximum amount of BTC to use
 
 //Send email reports when orders are placed
 $emailRCPTo = "SET IN KEY.PHP INCLUDE FILE";
-$btceKey = 'FILL ME'; // your API-key
-$btceSecret = 'FILL ME'; // your Secret-key
+$btceKey = 'RZU7TOB6-MWDJLINZ-XJYTPYHY-MROINI7A-0YEOEM75'; // your API-key
+$btceSecret = '2ee951f0b432e2fb606aced4f8cf9325a2eb9db2407452b839eb420eef9713a0'; // your Secret-key
 $emailSubject = "BTCbot Trade on {$exchange}";
  
 
